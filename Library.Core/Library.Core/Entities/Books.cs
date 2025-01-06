b7786822fffa133f109e738ce209500e5d1c22cb
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-01-06 20:18:36 +0100

    Create Books entity + SQL

---
+++ Library.Core/Library.Core/Entities/Books.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Core.Entities
+{
+    public class Books
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BID { get; set; }
+        public Guid BGID { get; set; }
+        public Guid BAuthorGID { get; set; }
+        public Guid BPublisherGID { get; set; }
+        public int BUID { get; set; }
+        public string BTitle { get; set; }
+        public string BISBN { get; set; }
+        public int BGenre { get; set; } //ToDo - set Enum insteadof int
+        public string BLanguage { get; set; }
+        public string? BDescription { get; set; }
+    }
+}
