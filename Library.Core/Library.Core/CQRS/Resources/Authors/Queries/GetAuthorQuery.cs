49543ae362efd49342475c1cdb84bb81b509f001
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-01-09 18:03:21 +0100

    CQRS queries

---
+++ Library.Core/Library.Core/CQRS/Resources/Authors/Queries/GetAuthorQuery.cs
@@ -0,0 +1,10 @@
+﻿using Library.Core.Models.ViewModels.AuthorsViewModels;
+using Library.Core.CQRS.Abstraction.Queries;
+
+namespace Library.Core.CQRS.Resources.Authors.Queries
+{
+    public class GetAuthorQuery : IQuery<AuthorViewModel>
+    {
+        public Guid AGID { get; set; }
+    }
+}
---
+++ Library.Core/Library.Core/CQRS/Resources/Authors/Queries/GetAuthorsQuery.cs
@@ -0,0 +1,11 @@
+﻿using Library.Core.Models.ViewModels.AuthorsViewModels;
+using Library.Core.CQRS.Abstraction.Queries;
+
+namespace Library.Core.CQRS.Resources.Authors.Queries
+{
+    public class GetAuthorsQuery : IQuery<AuthorsListViewModel>
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
