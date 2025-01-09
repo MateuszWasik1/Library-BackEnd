1cb103d7af9cbafd22bf5c7b27e9af8bf71274f2
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-01-09 18:02:04 +0100

    CQRS commands

---
+++ Library.Core/Library.Core/CQRS/Resources/Authors/Commands/DeleteAuthorCommand.cs
@@ -0,0 +1,9 @@
+﻿using Library.Core.CQRS.Abstraction.Commands;
+
+namespace Library.Core.CQRS.Resources.Authors.Commands
+{
+    public class DeleteAuthorCommand : ICommand
+    {
+        public Guid AGID { get; set; }
+    }
+}
