0e17dff6c4f0fde7db520c7af061d6ab67d0a919
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-01-10 17:37:17 +0100

    CQRS Queries

---
+++ Library.Core/Library.Core/CQRS/Resources/Publishers/Queries/GetPublishersQuery.cs
@@ -0,0 +1,11 @@
+﻿using Library.Core.Models.ViewModels.PublishersViewModels;
+using Library.Core.CQRS.Abstraction.Queries;
+
+namespace Library.Core.CQRS.Resources.Publishers.Queries
+{
+    public class GetPublishersQuery : IQuery<PublishersListViewModel>
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
