1640c058d0438beb3c7cd50dc5b29e7a4bada7e5
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-01-10 17:26:27 +0100

    Entity + SQL

--- Library.Core/Library.Core/Models/SQL/Init.sql
+++ Library.Core/Library.Core/Models/SQL/Init.sql
@@ -40,3 +40,13 @@ CREATE TABLE Books (
 	BLanguage nvarchar(255) NOT NULL,
 	BDescription nvarchar(2000) NULL,
 );
+
+CREATE TABLE Publishers (
+	PID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    PGID uniqueidentifier NOT NULL,
+    PName nvarchar(255) NOT NULL,
+    PCountry nvarchar(255) NULL,
+    PCity nvarchar(255) NULL,
+    PEmail nvarchar(255) NULL,
+    PPhone nvarchar(255) NULL,
+);
