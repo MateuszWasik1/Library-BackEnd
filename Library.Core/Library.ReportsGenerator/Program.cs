c8f5a798818b375347cc6fad11e1da2a64ca1b71
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2025-02-02 20:10:25 +0100

    Reports Generator

---
+++ Library.Core/Library.ReportsGenerator/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Library.Core; 
+using Library.ReportsGenerator.Reports;
+
+class Program
+{
+    private readonly IServiceProvider serviceProvider;
+
+    public Program(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public void Run()
+    {
+        Timer timer = new Timer(Callback, null, 0, 60000);
+    }
+
+    private void Callback(object state)
+    {
+        var date = DateTime.Now;
+
+        if (date.Hour == 20 && date.Minute == 0)
+        {
+            var generator = serviceProvider.GetRequiredService<DataSumReport>();
+            generator.GenerateDataSumReport();
+        }
+    }
+
+    public static void Main()
+    {
+        var serviceProvider = ConfigureServices();
+        var program = new Program(serviceProvider);
+
+        program.Run();
+
+        Console.WriteLine("Program działa. Naciśnij Enter, aby zakończyć...");
+        Console.ReadLine();
+    }
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddDbContext<DataContext>(options =>
+            options.UseSqlServer("Server=.\\SqlExpress; Database=Library; Trusted_Connection=true; TrustServerCertificate=true;"));
+
+        serviceCollection.AddTransient<DataSumReport>();
+
+        return serviceCollection.BuildServiceProvider();
+    }
+}
